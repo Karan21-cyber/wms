dbb03158a4e77aadd14aa387ccedaa2e0972fbf6
Karan Chaudhary <tharukaran930@gmail.com>
2024-05-16 14:00:45 +0545

    cors implemented

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -13,10 +13,16 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
     const path = "/api/socket/io";
     const httpServer: NetServer = res.socket.server as any;
+
     const io = new ServerIO(httpServer, {
       path,
       addTrailingSlash: false,
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
     });
+
     io.on("connection", (s) => {
       s.on("create-room", (fileId) => {
         s.join(fileId);
